f43c29bdf8c827107304fde8af27b665b95e5956
Joshua Clarence Sadsad <joshsad2@gmail.com>
2019-08-31 04:10:33 +0000

    Automatically backed up by Learn

--- app/controllers/store_admin_controller.rb
+++ app/controllers/store_admin_controller.rb
@@ -5,7 +5,7 @@ class StoreAdminController < ApplicationController
   end
 
   def orders
-    render :layout => "orders_administration"
+    render :layout => "order_administration"
   end
 
   def invoice
